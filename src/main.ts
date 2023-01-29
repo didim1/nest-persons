c1024ef56f721cc8f4660fc69a999df251c95b77
didim1 <dimasseptian.ns@gmail.com>
2023-01-30 06:25:31 +0700

    cors anjing 2

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,21 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors({
+  //   origin: ['http://localhost:5173'],
+  //   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD', 'PUT'],
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
+  let domains = ['http://localhost:5173'];
   app.enableCors({
-    origin: ['http://localhost:5173'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD', 'PUT'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    origin: function (origin, callback) {
+      if (!origin || domains.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
   });
   await app.listen(3000);
 }
