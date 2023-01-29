10db993ec4774f3faaed0f47fc7e007765ea406d
didim1 <dimasseptian.ns@gmail.com>
2023-01-30 06:28:33 +0700

    mari kita bedaaahh

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors({
-  //   origin: ['http://localhost:5173'],
-  //   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD', 'PUT'],
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  // });
-  let domains = ['http://localhost:5173'];
   app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || domains.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD', 'PUT'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
+  // let domains = ['http://localhost:5173'];
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (!origin || domains.indexOf(origin) !== -1) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  // });
   await app.listen(3000);
 }
 bootstrap();
