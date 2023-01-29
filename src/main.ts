ce71b5e6f323b28db54287d76d5ea07654e4e150
didim1 <dimasseptian.ns@gmail.com>
2023-01-29 22:27:31 +0700

    some cors stuffs

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD', 'PUT'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   await app.listen(3000);
 }
